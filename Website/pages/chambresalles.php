66b1d019ab9c97fdb4800517bd96726184e829bb
hugoblois49 <107034932+hugoblois49@users.noreply.github.com>
2022-12-17 10:06:09 +0100

    Correction problème mélange footer and header

--- Website/pages/chambresalles.php
+++ Website/pages/chambresalles.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <title>Chambres et Salles</title>
-    <link rel="stylesheet" href="css/style.css" >
+    <link rel="stylesheet" href="/css/style.css" >
   </head>
 
   <?php include "header.php" ?>
--- Website/pages/reservation.php
+++ Website/pages/reservation.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <title>Réservation</title>
-    <link rel="stylesheet" href="css/style.css" >
+    <link rel="stylesheet" href="/css/style.css" >
   </head>
 
   <?php include "header.php" ?>
