16c2446d720a743b603da686742998e180471e73
TheoLrt <theo.lurat@gmail.com>
2022-12-20 12:46:53 +0100

    ajout du core adapté et relié a la DB

---
+++ Website/core/core.php
@@ -0,0 +1,35 @@
+<?php
+
+    echo '<p> Hello </p>';
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    ini_set('display_startup_errors', 'On');
+
+
+    $user = 'admin';
+    $password = 'mdpgite';
+    $db = 'gite';
+    $host = 'localhost';
+    $port = 3306;
+
+    $link = mysqli_init();
+    $success = mysqli_real_connect($link,$host,$user,$password,$db,$port);
+
+    $infoBdd = [
+        'server' => 'localhost',
+        'login' => 'admin',
+        'password' => 'mdpgite',
+        'db_name' => 'gite',
+    ];
+    
+    $mysqli = new mysqli($infoBdd['server'], $infoBdd['login'],
+                            $infoBdd['password'],$infoBdd['db_name'],3306);
+    if ($mysqli->connect_errno) {
+     exit("Problème de connexion à la BDD");
+    }
+
+    $result = $mysqli->query("INSERT INTO `Actualites` (`Id`,`Nom`,`Description`) 
+                            VALUES (2,'Mariage', 'Premier mariage Champetre');");
+
+?>
