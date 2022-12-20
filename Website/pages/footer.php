3be35b40dfa22a027af67b04ee34d2d8e68f4c06
TheoLrt <theo.lurat@gmail.com>
2022-12-20 12:47:54 +0100

    Ajout du site web à jour avec test DB

---
+++ Website/pages/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer-basic">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="../css/footer.css">
+    </head>
+
+    <div class="social">
+        <a href="https://www.facebook.com/Les-Logis-de-Beaulieu-929752433837612"><i class="icon ion-social-facebook"></i></a>
+        <a href="https://www.instagram.com/leslogisdebeaulieu/"><i class="icon ion-social-instagram"></i></a>
+        <a href="#"><i class="icon ion-social-twitter"></i></a>
+    </div>
+    
+    <ul>
+        <li><a href="avis.php">Nous Contacter</a></li>
+        <li><a href="#">Politique de confidentialité</a></li>
+        <li><a href="#">Mentions légales</a></li>
+    </ul>
+
+    <p class="copyright">Les logis de Beaulieu © 2022</p>
+
+</footer>
---
+++ Website/pages/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Les Logis de Beaulieu</title>
+    <link rel="stylesheet" href="../css/header.css" >
+  </head>
+
+  <body>
+    <div class="header">
+        <p>Les Logis de Beaulieu</p>
+        <hr>
+        <div class="button">
+            <a href="index.php">    Accueil     </a>
+            <a href="actualites.php">    Actualités     </a>
+            <a href="chambresalles.php">    Chambres et Salles     </a>
+            <a href="reservation.php">    Réservation en ligne     </a>
+            <a href="avis.php">    Avis et Contact     </a>
+        </div>
+    </div>
+  </body>
+
+</html>
