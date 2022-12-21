0c9cfc70884336d507206e7b512950a6f4588655
TheoLrt <theo.lurat@gmail.com>
2022-12-21 09:59:59 +0100

    adapte core.php a prod

--- Website/core/core.php
+++ Website/core/core.php
@@ -7,29 +7,29 @@
     ini_set('display_startup_errors', 'On');
 
 
-    $user = 'root';
-    $password = 'root';
+    $user = 'admin';
+    $password = 'mdpgite';
     $db = 'gite';
     $host = 'localhost';
-    $port = 8889;
+    $port = 3306;
 
     $link = mysqli_init();
     $success = mysqli_real_connect($link,$host,$user,$password,$db,$port);
 
     $infoBdd = [
         'server' => 'localhost',
-        'login' => 'root',
-        'password' => 'root',
+        'login' => 'admin',
+        'password' => 'mdpgite',
         'db_name' => 'gite',
     ];
     
     $mysqli = new mysqli($infoBdd['server'], $infoBdd['login'],
-                            $infoBdd['password'],$infoBdd['db_name'],8889);
+                            $infoBdd['password'],$infoBdd['db_name'],3306);
     if ($mysqli->connect_errno) {
      exit("Problème de connexion à la BDD");
     }
 
-    $result = $mysqli->query("INSERT INTO `actualites` (`Id`,`Nom`,`Description`) 
+    $result = $mysqli->query("INSERT INTO `Actualites` (`Id`,`Nom`,`Description`) 
                             VALUES (2,'Mariage', 'Premier mariage Champetre');");
 
 ?>
