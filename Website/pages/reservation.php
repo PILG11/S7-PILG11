dd2301f0d771f9293265b12994dfba177ff883fc
hugoblois49 <107034932+hugoblois49@users.noreply.github.com>
2022-12-26 11:40:11 +0100

    Debut dev

--- Website/pages/reservation.php
+++ Website/pages/reservation.php
@@ -3,13 +3,29 @@
   <head>
     <meta charset="utf-8" />
     <title>Réservation</title>
-    <link rel="stylesheet" href="/css/style.css" >
+    <link rel="stylesheet" href="../css/reservation.css" >
   </head>
 
   <?php include "header.php" ?>
 
   <body>
-    <p>Réservation</p>
+
+    <p style="text-align: center; font-size:30px; margin-top:3%; margin-bottom:3%;"> Nos Services :</p>
+
+    <div class="onglets">
+      <ul class="choix">
+        <li class="option">
+          Chambres
+        </li>
+        <li class="option">
+          Salle des fêtes
+        </li>
+        <li class="option">
+          Formules
+        </li>
+      </ul>
+    </div>
+
   </body>
 
   <?php include 'footer.php' ?>
