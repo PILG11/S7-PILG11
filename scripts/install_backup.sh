35ac49ac0b0e218c04571b77837a1a73be65c146
TheoLrt <theo.lurat@gmail.com>
2022-12-27 14:51:27 +0100

    modif nom des fichiers a sauvegarder

--- scripts/install_backup.sh
+++ scripts/install_backup.sh
@@ -12,6 +12,7 @@ DB_USER="admin"
 DB_PASSWD="mdpgite"
 
 BACKUP_FILE="/vagrant/backups"
+BACKUP_NAME="$DATE-$DB_NAME"
 
 # Nombre de jours à garder les dossiers (seront effacés après X jours)
 RETENTION=14
@@ -28,11 +29,9 @@ apt-get install $APT_OPT \
   >> $LOG_FILE 2>&1
 
 echo "=> [2]: Retrieving the database and storing it"
-# Créer un dossier de sauvegarde s'il n'existe pas déjà
-mkdir -p $BACKUP_FILE/$DATE
 
 # Dumb the database and gzip the .sql file
-mysqldump --force --opt --user=$DB_USER -p$DB_PASSWD --skip-lock-tables --events --databases $DB_NAME > "$BACKUP_FILE/$DATE/$DB_NAME.sql"
+mysqldump --force --opt --user=$DB_USER -p$DB_PASSWD --skip-lock-tables --events --databases $DB_NAME > "$BACKUP_FILE/$BACKUP_NAME.sql"
 
 # Remove files older than X days
 find $BACKUP_FILE/* -mtime +$RETENTION -delete
