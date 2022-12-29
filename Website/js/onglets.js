de4060b449db90863b527a67acf70e94eb97f374
hugoblois49 <107034932+hugoblois49@users.noreply.github.com>
2022-12-29 16:28:50 +0100

    Finition du développement du front-end

--- Website/js/onglets.js
+++ Website/js/onglets.js
@@ -1,26 +1,61 @@
+const indication = document.getElementsByName("indication");
 
-const chambreDiv = document.getElementById("Chambres"); 
-const salleDesFetesDiv = document.getElementById("SalleDesFetes");
-const formulesDiv = document.getElementById("Formules");
+function onglet(option){
+
+    const chambreDiv = document.getElementById("Chambres"); 
+    const salleDesFetesDiv = document.getElementById("SalleDesFetes");
+    const formulesDiv = document.getElementById("Formules");
 
 
-function onglet(type){
+    // 1 --- CHAMBRES
+    // 2 --- SALLE DES FETES
+    // 3 --- FORMULES
 
-    alert(type.style);
+    if(option === 'chambres'){
 
-    if(type == "chambres"){
-        chambreDiv.style.display = flex;
-        salleDesFetesDiv.style.display = none;
-        formulesDiv.style.display = none;
+        indication[0].classList.remove("notActive");
+        indication[0].classList.add("active");
+
+        indication[1].classList.remove("active");
+        indication[2].classList.remove("active");
+
+        indication[1].classList.add("notActive");
+        indication[2].classList.add("notActive");
+
+        formulesDiv.style.display = "none";
+        salleDesFetesDiv.style.display = "none";
+        chambreDiv.style.display = "flex";
     }
-    else if(type = "SalleDesFetes"){
-        chambreDiv.style.display = none;
-        salleDesFetesDiv.style.display = flex;
-        formulesDiv.style.display = none;
+
+    if(option === 'salledesfetes'){
+
+        indication[1].classList.remove("notActive");
+        indication[1].classList.add("active");
+
+        indication[2].classList.remove("active");
+        indication[0].classList.remove("active");
+
+        indication[0].classList.add("notActive");
+        indication[2].classList.add("notActive");
+
+        formulesDiv.style.display = "none";
+        salleDesFetesDiv.style.display = "flex";
+        chambreDiv.style.display = "none";
     }
-    else{
-        chambreDiv.style.display = none;
-        salleDesFetesDiv.style.display = none;
-        formulesDiv.style.display = flex;
+
+    if(option === 'formules'){
+
+        indication[2].classList.remove("notActive");
+        indication[2].classList.add("active");
+
+        indication[1].classList.remove("active");
+        indication[0].classList.remove("active");
+
+        indication[1].classList.add("notActive");
+        indication[0].classList.add("notActive");
+
+        formulesDiv.style.display = "flex";
+        salleDesFetesDiv.style.display = "none";
+        chambreDiv.style.display = "none";
     }
 }
