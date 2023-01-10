119a890eb0303a69bbdbe87b1cc0691d0d136a79
TheoLrt <theo.lurat@gmail.com>
2023-01-10 17:05:36 +0100

    edit upload backup script

--- scripts/upload_backup.sh
+++ scripts/upload_backup.sh
@@ -6,8 +6,13 @@ DB_PASSWD="mdpgite"
 BACKUP_FILE="/vagrant/backups"
 BACKUP_NAME="$DATE-$DB_NAME"
 
+AWS_CONF_FILE="/vagrant/scripts/config_aws.sh"
+
 #Dump actual database
 mysqldump --force --opt --user=$DB_USER -p$DB_PASSWD --skip-lock-tables --events --databases $DB_NAME > "$BACKUP_FILE/$BACKUP_NAME.sql"
+
+#Config AWS credentials
+bash $AWS_CONF_FILE
 
 #Upload database file to AWS
 aws s3 cp "$BACKUP_FILE/$BACKUP_NAME.sql" s3://pilg11-db-backup/
