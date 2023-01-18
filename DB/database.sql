b69ccbdc683c0e7874b8d41e95e1a7eb49b12c3b
teosueur <97182374+teosueur@users.noreply.github.com>
2023-01-18 10:39:44 +0100

    PILG11-33 comment# bonne BDD

--- DB/database.sql
+++ DB/database.sql
@@ -2,65 +2,65 @@ CREATE DATABASE gite;
 
 USE gite;
 
-CREATE TABLE inventaire (
-    Id INTEGER PRIMARY KEY,
-    Materiel VARCHAR(20),
-    Quantite INTEGER,
-    Etat ENUM('neuf', 'bon', 'moyen', 'mauvais'),
-    Lieu VARCHAR(20)
+CREATE TABLE Inventaire (
+    id INTEGER PRIMARY KEY,
+    materiel VARCHAR(20),
+    quantite INTEGER,
+    etat ENUM('neuf', 'bon', 'moyen', 'mauvais'),
+    lieu VARCHAR(20)
 );
 
 CREATE TABLE Chambres (
-    Id INTEGER PRIMARY KEY,
-    Nom VARCHAR(20),
-    Type VARCHAR(20),
-    Places INTEGER,
-    Douche BOOLEAN,
-    Image VARCHAR(255)
+    id INTEGER PRIMARY KEY,
+    nom VARCHAR(20),
+    type VARCHAR(20),
+    places INTEGER,
+    douche BOOLEAN,
+    image VARCHAR(255)
 );
 
 CREATE TABLE Clients (
-    Id INTEGER PRIMARY KEY,
-    Nom VARCHAR(20),
-    Prenom VARCHAR(20),
-    Email VARCHAR(50),
-    Numero VARCHAR(20)
+    id INTEGER PRIMARY KEY,
+    nom VARCHAR(20),
+    prenom VARCHAR(20),
+    email VARCHAR(50),
+    numero VARCHAR(20)
 );
 
 CREATE TABLE Salles (
-    Id INTEGER PRIMARY KEY,
-    Nom VARCHAR(20),
-    Type VARCHAR(20),
-    Capacite INTEGER,
-    Image VARCHAR(255)
+    id INTEGER PRIMARY KEY,
+    nom VARCHAR(20),
+    type VARCHAR(20),
+    capacite INTEGER,
+    image VARCHAR(255)
 );
 
 CREATE TABLE Actualites (
-    Id INTEGER PRIMARY KEY,
-    Nom VARCHAR(20),
-    Description VARCHAR(255),
-    PremièreImage VARCHAR(255),
-    SecondeImage VARCHAR(255),
-    TroisiemeImage VARCHAR(255),
-    QuatriemeImage VARCHAR(255),
+    id INTEGER PRIMARY KEY,
+    nom VARCHAR(20),
+    description VARCHAR(255),
+    premièreImage VARCHAR(255),
+    secondeImage VARCHAR(255),
+    troisiemeImage VARCHAR(255),
+    quatriemeImage VARCHAR(255),
     date DATE
 );
 
 CREATE TABLE Reservations (
-    Id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     Type VARCHAR(20),
-    Valide BOOLEAN,
-    Date DATE,
-    Duree INTEGER,
-    Chambres INTEGER,
-    Salles INTEGER,
-    Client INTEGER,
-    FOREIGN KEY (Chambres) REFERENCES Chambres(Id),
-    FOREIGN KEY (Salles) REFERENCES Salles(Id),
-    FOREIGN KEY (Client) REFERENCES Clients(Id)
+    valide BOOLEAN,
+    date DATE,
+    duree INTEGER,
+    chambres INTEGER,
+    salles INTEGER,
+    client INTEGER,
+    FOREIGN KEY (chambres) REFERENCES chambres(id),
+    FOREIGN KEY (salles) REFERENCES salles(id),
+    FOREIGN KEY (client) REFERENCES clients(id)
 );
 
-CREATE TABLE avis (
+CREATE TABLE Avis (
   id INT PRIMARY KEY,
   nom VARCHAR(255),
   prenom VARCHAR(255),
@@ -69,9 +69,15 @@ CREATE TABLE avis (
   etoile INT
 );
 
-INSERT INTO `actualites` (`Id`, `Nom`, `Description`, `PremièreImage`, `SecondeImage`, `TroisiemeImage`, `QuatriemeImage`, `date`) VALUES
+INSERT INTO `Actualites` (`Id`, `Nom`, `Description`, `PremièreImage`, `SecondeImage`, `TroisiemeImage`, `QuatriemeImage`, `date`) VALUES
 (1, 'Mariage Champêtre', 'La saison des mariages n’est pas fini, mariage d’automne ce week-end.\r\n\r\n#mariagechampetre #maineetloire #bassesvalleesangevines #rivesduloirenanjou #salledereceptionmariage #salledereceptionavechebergements', '../img/Actualites/premier_post/premier_post.jpg', '../img/Actualites/premier_post/premier_post_2.jpg', '../img/Actualites/premier_post/premier_post_3.jpg', '../img/Actualites/premier_post/premier_post_4.jpg', '2022-10-08'),
 (2, 'Les extérieurs', 'Nos espaces extérieurs et la salle en mode banquet #salledereception #bibou #mariagechampetre #bassesvalleesangevines #romaintouzé #hebergementsinsolites', '../img/Actualites/second_post/second_post.jpg', '../img/Actualites/second_post/second_post_2.jpg', '../img/Actualites/second_post/second_post_3.jpg', '../img/Actualites/second_post/second_post_4.jpg', '2022-07-24'),
 (3, 'Mariage au logis', 'Mariage au logis #mariage #decomariage #lydiefleurs #salledereception #maineetloire #bassesvalleesangevines #mariagehebergement', '../img/Actualites/troisieme_post/troisieme_post.jpg', '../img/Actualites/troisieme_post/troisieme_post_2.jpg', '../img/Actualites/troisieme_post/troisieme_post_3.jpg', '../img/Actualites/troisieme_post/troisieme_post_4.jpg', '2022-07-10'),
 (4, 'Robe de mariée', 'Robe de mariée de Blandine\r\n\r\n#robedemariee #mariage #mariagechic #cheval #salledereception #hebergementinsolite', '../img/Actualites/quatrieme_post/quatrieme_post.jpg', NULL, NULL, NULL, '2022-06-26');
+
+INSERT INTO `avis` (`id`, `nom`, `prenom`, `commentaire`, `email`, `note`) VALUES
+(4, 'ThÃ©o', 'SUEUR', 'Le logis fut fameux.', 'ttsueur62600@gmail.com', '3'),
+(8, 'Blois', 'Hugo', 'Le logis Ã©tait incroyable.', 'hugoblois@gmail.com', '5'),
+(9, 'Guilpin', 'Erwan', 'Les hÃ´tes nous ont trÃ¨s trÃ¨s bien reÃ§us.', 'erwanguilpin@gmail.com', '5'),
+(10, 'Souletis', 'Clovis', 'Amazing !!!!!!!!!!', 'cloclo@gmail.com', '5');
 
