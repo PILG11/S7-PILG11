a42ce5f84513c86f2a9683131d6db121ef81e760
Wawone <erwanguilpin@gmail.com>
2023-01-18 10:02:59 +0100

    PILG11-54 Exemple pull et push bdd

--- Java/modele/basededonnees/RequeteBdd.java
+++ Java/modele/basededonnees/RequeteBdd.java
@@ -6,7 +6,7 @@ public class RequeteBdd {
 
     public static void main(String[] args) {
         RequeteBdd service = new RequeteBdd();
-        service.verifyConnection();
+        service.tryPull(service.dbConnection);
     }
 
     private ConnectionBdd dbConnection;
@@ -15,12 +15,33 @@ public class RequeteBdd {
         dbConnection = new ConnectionBdd();
     }
 
-    public void verifyConnection() {
+    public void tryRequete(ConnectionBdd dbConnection){
         dbConnection.openConnection();
         try {
-            DatabaseMetaData meta = dbConnection.getConnection().getMetaData();
-            System.out.println("Connexion établie avec la base de données : " + meta.getURL());
-            System.out.println("Driver utilisé : " + meta.getDriverName());
+            String query = "INSERT INTO `clients`(`Id`, `Nom`, `Prenom`, `Email`, `Numero`) VALUES ('2','Hugo','Blois','blois@gmail.com','0672346666')";
+            PreparedStatement stmt = dbConnection.getConnection().prepareStatement(query);
+            stmt.executeUpdate();
+            System.out.println("L'actualité a été ajoutée avec succès!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dbConnection.closeConnection();
+        }
+    }
+
+    public void tryPull(ConnectionBdd dbConnection){
+        dbConnection.openConnection();
+        try {
+            String query = "SELECT * FROM clients";
+            PreparedStatement stmt = dbConnection.getConnection().prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("Id"));
+                System.out.println("Nom: " + rs.getString("Nom"));
+                System.out.println("Prenom: " + rs.getString("Prenom"));
+                System.out.println("Email: " + rs.getString("Email"));
+                System.out.println("Téléphone: " + rs.getString("Numero"));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
