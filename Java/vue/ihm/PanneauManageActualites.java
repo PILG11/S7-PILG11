517a4f10a4da8cd990bf0f893e088b4dfd52d7d9
Wawone <erwanguilpin@gmail.com>
2023-01-19 11:49:34 +0100

    PILG11-54 Mise en page Manage Actu

--- Java/vue/ihm/PanneauManageActualites.java
+++ Java/vue/ihm/PanneauManageActualites.java
@@ -1,6 +1,14 @@
 package Java.vue.ihm;
 
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import Java.controleur.actions.ActionChangerPanneau;
@@ -9,9 +17,40 @@ public class PanneauManageActualites extends JPanel{
 
     private MainJFrame mainJFrame;
 
+    List<String> listTitre = Arrays.asList("titre1", "titre2", "titre3");
+    List<String> listDescr = Arrays.asList("descr1", "descr2", "descr3");
+    List<String> listDate = Arrays.asList("date1","date2","date3");
+
     public PanneauManageActualites(MainJFrame mainJFrame){
         this.mainJFrame = mainJFrame;
+        GridLayout grid = new GridLayout(10, 1);
+        this.setLayout(grid);
+        super.setPreferredSize(new Dimension(600,500));
 
+        afficherActu();
+        privateButton();
+    }
+
+    private void afficherActu(){
+        for (int i = 0; i < this.listTitre.size(); i++) {
+
+            JPanel ligne = new JPanel();
+            ligne.setLayout(new GridLayout(1, 3));
+
+            JLabel titre = new JLabel(this.listTitre.get(i));
+            JLabel descr = new JLabel(this.listDescr.get(i));
+            JLabel date = new JLabel(this.listDate.get(i));
+            JButton supprButton = new JButton();
+            supprButton.setText("Supprimer");
+
+            ligne.add(titre);
+            ligne.add(descr);
+            ligne.add(date);
+            ligne.add(supprButton);
+            this.add(ligne);
+        }
+    }
+    private void privateButton(){
         JButton buttonRetour = new JButton(new ActionChangerPanneau(this.mainJFrame, "Retour", "actualites"));
         buttonRetour.setText("Retour");
         this.add(buttonRetour);
