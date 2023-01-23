57b67b231473ff18d0c976d3980ed7795e5aefd4
TheoLrt <theo.lurat@gmail.com>
2023-01-23 14:07:21 +0100

    fix code hugo

--- Website/core/formulaire_reservation.php
+++ Website/core/formulaire_reservation.php
@@ -1,72 +1,83 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <title>Réservation</title>
-  </head>
-    <body>
-<?php 
+</head>
 
-include_once 'core.php';
-$form = false;
-$num = false;
-$emailVerif = false;
-$requete = false;
+<body>
+    <?php
 
-function get_second_word($string) {
-    $words = explode("-", $string);
-    return trim($words[1]);
-}
+    include_once 'core.php';
+    $form = false;
+    $num = false;
+    $emailVerif = false;
+    $requete = false;
 
-function isValidEmail($email) {
-    $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
-    return preg_match($pattern, $email);
-}
+    function get_second_word($string)
+    {
+        $words = explode("-", $string);
+        return trim($words[1]);
+    }
 
-function formatDateToEnglish($date) {
-    $french_months = array("Janvier", "Février", "Mars", "avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
-    $english_months = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-    $new_date = str_replace($french_months, $english_months, $date);
-    return $new_date;
-}
+    function isValidEmail($email)
+    {
+        $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+        return preg_match($pattern, $email);
+    }
 
-function formatDate($date) {
-    date_default_timezone_set('Europe/Paris');
-    $date = new DateTime($date);
-    $day = $date->format("j");
-    $month = $date->format("F");
-    $year = $date->format("Y");
-    return "$day $month, $year";
-}
+    function formatDateToEnglish($date)
+    {
+        $french_months = array("Janvier", "Février", "Mars", "avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+        $english_months = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        $new_date = str_replace($french_months, $english_months, $date);
+        return $new_date;
+    }
 
-function unformatDate($formattedDate) {
-    $date = DateTime::createFromFormat("j F, Y", $formattedDate);
-    return $date->format("Y-m-d");
-}
+    function formatDate($date)
+    {
+        date_default_timezone_set('Europe/Paris');
+        $date = new DateTime($date);
+        $day = $date->format("j");
+        $month = $date->format("F");
+        $year = $date->format("Y");
+        return "$day $month, $year";
+    }
 
-function calculer_nombre_jours($date1, $date2) {
-    $date1 = strtotime($date1);
-    $date2 = strtotime($date2);
-    $diff = abs($date2 - $date1);
-    $result = floor($diff / (60*60*24));
-    return $result;
-}
+    function unformatDate($formattedDate)
+    {
+        $date = DateTime::createFromFormat("j F, Y", $formattedDate);
+        return $date->format("Y-m-d");
+    }
 
-function isValidPhoneNumber($phoneNumber) {
-    $pattern = '/^[+]?[0-9]{10,14}$/';
-    return preg_match($pattern, $phoneNumber);
-}
+    function calculer_nombre_jours($date1, $date2)
+    {
+        $date1 = strtotime($date1);
+        $date2 = strtotime($date2);
+        $diff = abs($date2 - $date1);
+        $result = floor($diff / (60 * 60 * 24));
+        return $result;
+    }
+
+    function isValidPhoneNumber($phoneNumber)
+    {
+        $pattern = '/^[+]?[0-9]{10,14}$/';
+        return preg_match($pattern, $phoneNumber);
+    }
 
     //-------------Gérer les réservations ------------------
 
-    if(isset($_POST['submit']) && $_POST['submit'] == 1){
-        if($_POST['nom'] != NULL && $_POST['prenom'] != NULL && $_POST['email'] != NULL && 
-        $_POST['numero'] != NULL && $_POST['event'] != NULL && $_POST['chambre'] != NULL && $_POST['salle'] != NULL
-        && $_POST['input_from'] != NULL && $_POST['input_to'] != NULL){
-            
-            
+    if (isset($_POST['submit']) && $_POST['submit'] == 1) {
+        if (
+            $_POST['nom'] != NULL && $_POST['prenom'] != NULL && $_POST['email'] != NULL &&
+            $_POST['numero'] != NULL && $_POST['event'] != NULL && $_POST['chambre'] != NULL && $_POST['salle'] != NULL
+            && $_POST['input_from'] != NULL && $_POST['input_to'] != NULL
+        ) {
+
+
             $form = false;
-            
+
             $nom = $_POST['nom'];
             $prenom = $_POST['prenom'];
             $email = $_POST['email'];
@@ -84,80 +95,75 @@ function isValidPhoneNumber($phoneNumber) {
 
             // -----------CLIENT-------------------
 
-            $result = $mysqli->query("SELECT * FROM clients WHERE nom LIKE '".$nom."' AND email LIKE '".$email."';");
+            $result = $mysqli->query("SELECT * FROM Clients WHERE nom LIKE '" . $nom . "' AND email LIKE '" . $email . "';");
             $row = $result->fetch_assoc();
 
 
-                //------------Créer un CLIENT-------
-            if($row === NULL){
+            //------------Créer un CLIENT-------
+            if ($row === NULL) {
 
-                $result = $mysqli->query("SELECT max(id) FROM clients");
+                $result = $mysqli->query("SELECT max(id) FROM Clients");
 
                 if (!$result) {
                     exit($mysqli->error);
-                }
-                else{
+                } else {
                     $row = $result->fetch_assoc();
                     $idClient = $row['max(id)'] + 1;
                 }
             }
-            
-                //------------Réutilise un CLIENT-------
-            else{
 
-                $result = $mysqli->query("SELECT clients.id FROM clients WHERE email LIKE '".$email."';");
+            //------------Réutilise un CLIENT-------
+            else {
+
+                $result = $mysqli->query("SELECT Clients.id FROM Clients WHERE email LIKE '" . $email . "';");
                 $row = $result->fetch_assoc();
                 $idClient = $row['id'];
             }
 
-            if(isValidPhoneNumber($numero)){
+            if (isValidPhoneNumber($numero)) {
 
                 $num = false;
 
-                if(isValidEmail($email)){
+                if (isValidEmail($email)) {
 
                     $emailVerif = false;
 
-                    $sql = "INSERT INTO clients(id, nom, prenom, email, numero)
-                    VALUES ('".$idClient."', '".$nom."', '".$prenom."'
-                    , '".$email."', '".$numero."')";
-                    
+                    $sql = "INSERT INTO Clients(id, nom, prenom, email, numero)
+                    VALUES ('" . $idClient . "', '" . $nom . "', '" . $prenom . "'
+                    , '" . $email . "', '" . $numero . "')";
+
                     $result = $mysqli->query($sql);
-                
-                }
-                else{
+                } else {
                     $emailVerif = true;
                 }
-            }
-            else{
+            } else {
                 $num = true;
             }
 
             //----------Salle------------------
 
             $list = array("Salle des fêtes", "Bar", "Le site", "Aucune");
-            $result = $mysqli->query("SELECT salles.id FROM salles WHERE nom LIKE '".$list[$salle - 1]."';");
+            $result = $mysqli->query("SELECT Salles.id FROM Salles WHERE nom LIKE '" . $list[$salle - 1] . "';");
             $idSalle = $result->fetch_assoc();
 
             //-----------Chambre----------------
 
             $list = array("Chêne", "Peuplier", "Abricot", "Forêt", "Le site", "Aucune");
-            $result = $mysqli->query("SELECT chambres.id FROM chambres WHERE nom LIKE '".$list[$chambre - 1]."';");
+            $result = $mysqli->query("SELECT Chambres.id FROM Chambres WHERE nom LIKE '" . $list[$chambre - 1] . "';");
             $idChambre = $result->fetch_assoc();
 
             //----------Reservations-------------
 
-            $result = $mysqli->query("SELECT Reservations.dateDebut FROM Reservations WHERE dateDebut LIKE '".$date_database."';");
+            $result = $mysqli->query("SELECT Reservations.dateDebut FROM Reservations WHERE dateDebut LIKE '" . $date_database . "';");
             $row = $result->fetch_assoc();
 
-            if($row === NULL){
-                
+            if ($row === NULL) {
+
                 $result = $mysqli->query("SELECT max(id) FROM Reservations");
 
                 if (!$result) {
                     exit($mysqli->error);
-                }
-                else{
+                } else {
                     $row = $result->fetch_assoc();
                     $idReservation = $row['max(id)'] + 1;
                 }
@@ -165,26 +171,23 @@ function isValidPhoneNumber($phoneNumber) {
                 $Duree = calculer_nombre_jours(unformatDate(formatDateToEnglish($input_from)), unformatDate(formatDateToEnglish($input_to)));
 
                 $sql = "INSERT INTO Reservations(id, evenement, valide, dateDebut, duree, chambres, client, salles)
-                VALUES ('".$idReservation."', '".$event."', 0, 
-                '".$date_database."'
-                , '".$Duree."'
-                , '".intval($idChambre['id'])."'
-                , '".intval($idClient)."'
-                , '".intval($idSalle['id'])."')";
+                VALUES ('" . $idReservation . "', '" . $event . "', 0, 
+                '" . $date_database . "'
+                , '" . $Duree . "'
+                , '" . intval($idChambre['id']) . "'
+                , '" . intval($idClient) . "'
+                , '" . intval($idSalle['id']) . "')";
 
                 $result = $mysqli->query($sql);
 
-                if($result){
+                if ($result) {
                     $requete = true;
                     echo   '<p style="text-align: center; font-size:15px; color:green;">Demande de réservation bien envoyée.</p>';
-                }
-                else{
+                } else {
                     $requete = false;
                 }
-                }
-            
-        }
-        else{
+            }
+        } else {
             $form = true;
         }
     }
@@ -193,15 +196,14 @@ function isValidPhoneNumber($phoneNumber) {
 
     $result = $mysqli->query("SELECT * FROM `Reservations`");
 
-    if(!$result) {
+    if (!$result) {
         exit($mysqli->error);
-    }
-    else{
+    } else {
 
         $row = $result->fetch_assoc();
         $datesReserves = array();
 
-        while($row != NULL){ 
+        while ($row != NULL) {
 
             $date = formatDate($row['dateDebut']);
 
@@ -210,76 +212,72 @@ function isValidPhoneNumber($phoneNumber) {
                 $dateSuivante = date('Y-m-d', strtotime($date . ' +1 day'));
                 $date = formatDate($dateSuivante);
             }
-        
-            $row = $result->fetch_assoc();
 
+            $row = $result->fetch_assoc();
         }
 
         $json = json_encode($datesReserves);
+    } ?>
 
-    }?>
-    
     <script>
+        document.addEventListener("DOMContentLoaded", function() {
 
-            document.addEventListener("DOMContentLoaded", function() {
+            var list = <?php echo $json; ?>;
+            var list_fr = formatDatesFr(list);
 
-                var list = <?php echo $json; ?>;
-                var list_fr = formatDatesFr(list);   
+            console.log(list_fr);
 
-                console.log(list_fr);
+            date_reservees();
 
-                date_reservees();
+            var calendriers = document.getElementsByClassName("picker__holder");
+            calendriers[0].addEventListener("click", date_reservees);
+            calendriers[1].addEventListener("click", date_reservees);
+
+            function date_reservees() {
 
                 var calendriers = document.getElementsByClassName("picker__holder");
-                calendriers[0].addEventListener("click", date_reservees);
-                calendriers[1].addEventListener("click", date_reservees);
+                for (var date of list_fr) {
+                    // console.log('[aria-label=' + '"' + date + '"' + ']')
+                    var date_selected_calendrier1 = calendriers[0].querySelector('[aria-label=' + '"' + date + '"' + ']');
+                    var date_selected_calendrier2 = calendriers[1].querySelector('[aria-label=' + '"' + date + '"' + ']');
 
-                function date_reservees() {
-
-                    var calendriers = document.getElementsByClassName("picker__holder");
-                    for (var date of list_fr){
-                        // console.log('[aria-label=' + '"' + date + '"' + ']')
-                        var date_selected_calendrier1 = calendriers[0].querySelector('[aria-label=' + '"' + date + '"' + ']');
-                        var date_selected_calendrier2 = calendriers[1].querySelector('[aria-label=' + '"' + date + '"' + ']');
-
-                        if(date_selected_calendrier1 != null){
-                            date_selected_calendrier1.classList.add("selected-date");
-                        }
-                        if(date_selected_calendrier2 != null){
-                            date_selected_calendrier2.classList.add("selected-date");
-                        }
+                    if (date_selected_calendrier1 != null) {
+                        date_selected_calendrier1.classList.add("selected-date");
+                    }
+                    if (date_selected_calendrier2 != null) {
+                        date_selected_calendrier2.classList.add("selected-date");
                     }
                 }
+            }
 
-                function formatDatesFr(dates) {
-                    const months = {
-                        'January,': 'Janvier',
-                        'February,': 'Février',
-                        'March,': 'Mars',
-                        'April,': 'Avril',
-                        'May,': 'Mai',
-                        'June,': 'Juin',
-                        'July,': 'Juillet',
-                        'August,': 'Août',
-                        'September,': 'Septembre',
-                        'October,': 'Octobre',
-                        'November,': 'Novembre',
-                        'December,': 'Décembre',
-                    };
+            function formatDatesFr(dates) {
+                const months = {
+                    'January,': 'Janvier',
+                    'February,': 'Février',
+                    'March,': 'Mars',
+                    'April,': 'Avril',
+                    'May,': 'Mai',
+                    'June,': 'Juin',
+                    'July,': 'Juillet',
+                    'August,': 'Août',
+                    'September,': 'Septembre',
+                    'October,': 'Octobre',
+                    'November,': 'Novembre',
+                    'December,': 'Décembre',
+                };
 
-                    return dates.map(date => {
-                        const parts = date.split(' ');
-                        const day = parts[0];
-                        const month = months[parts[1]];
-                        const year = parts[2];
+                return dates.map(date => {
+                    const parts = date.split(' ');
+                    const day = parts[0];
+                    const month = months[parts[1]];
+                    const year = parts[2];
 
-                        return `${day} ${month}, ${year}`;
-                    });
-                }
+                    return `${day} ${month}, ${year}`;
+                });
+            }
 
-            });
-    
-
+        });
     </script>
-    </body>
+</body>
+
 </html>
